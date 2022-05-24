900a6eddaa209f1a962ce703c43e294800e59e9c
Indirect91 <shhyc1001@gmail.com>
2022-05-24 18:05:52 +0900

    Added Minor loading images and logic
    로딩씬 이미지 및 퍼센트에 따른 애니메이션 추가

--- Assets/02.Scrips/SceneControllers/LoadingControl.cs
+++ Assets/02.Scrips/SceneControllers/LoadingControl.cs
@@ -7,6 +7,15 @@ using UnityEngine.SceneManagement;
 public class LoadingControl : MonoBehaviour
 {
     public Text loadingTxt;
+    public Image loadingImg;
+    
+    [SerializeField] 
+    private Sprite[] loadingImgSprites;
+    [SerializeField]
+    private int index = 0;
+    private float timer = 0;
+
+
     static string sceneToLoad;
     public static void LoadSceneWithLoading(string _sceneToLoad)
     {
@@ -18,31 +27,71 @@ public class LoadingControl : MonoBehaviour
     {
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
         asyncOperation.allowSceneActivation = false;
+        float toTest = 0.0f;
 
-        float timer = 0.0f;
-
-        while(asyncOperation.isDone ==false)
+        float indexScale = (float)loadingImgSprites.Length / 0.9f;
+        
+        while(index< loadingImgSprites.Length)
         {
-            yield return null;
-            if(asyncOperation.progress < 0.9f)
+            if (asyncOperation.progress < 0.9 && toTest * indexScale < index + 1)
+            //if (asyncOperation.progress<0.9 && asyncOperation.progress*indexScale<index+1)
             {
-                loadingTxt.text = asyncOperation.progress.ToString("00.00");
+                Debug.Log(toTest);
+                Debug.Log(index);
+                toTest += 0.1f;
+                Debug.Assert(true); //toFix
+                // yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(0.5f);
+
             }
-            else 
+            else if(asyncOperation.progress < 0.9 && asyncOperation.progress * indexScale > index + 1)
             {
-                timer += Time.unscaledDeltaTime;
-                loadingTxt.text = asyncOperation.progress.ToString();
-                if(timer>1.0f)
+                index++;
+                //Debug.Log(asyncOperation.progress * indexScale);
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            
+            loadingTxt.text = ((float)index * 90.0f / (float)loadingImgSprites.Length).ToString("00.00");
+            loadingImg.sprite = loadingImgSprites[index];
+           // yield return new WaitForSeconds(0.05f);
+        }
+
+
+        if (asyncOperation.isDone == false)
+        {
+            while (asyncOperation.isDone == false)
+            {
+                //   loadingImg.sprite = loadingImgSprites[index];
+                //   index = (index + 1) % loadingImgSprites.Length;
+
+
+
+                //yield return new WaitForSeconds(0.01f);
+                yield return null;
+                if (asyncOperation.progress < 0.9f)
                 {
-                    asyncOperation.allowSceneActivation = true;
-                    yield break;
+                    loadingTxt.text = (asyncOperation.progress * 100).ToString("00.00");
+                }
+                else
+                {
+                    timer += Time.unscaledDeltaTime;
+                    loadingTxt.text = asyncOperation.progress.ToString();
+                    if (timer > 1.0f)
+                    {
+                        asyncOperation.allowSceneActivation = true;
+                        yield break;
+                    }
                 }
             }
+            asyncOperation.allowSceneActivation = true;
+            yield break;
         }
     }
 
     void Start()
     {
+        
         StartCoroutine(SceneLoadProgress());
     }
 
